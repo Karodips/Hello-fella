0075efbf0652e34b8333d69df426fec5306ca872
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 13:12:54 +0300

    Update IDK.py

--- IDK.py
+++ IDK.py
@@ -4,7 +4,7 @@
 print('Enter numbers')  
 a = float(input())
 b = float(input())
-if (a == 0) or (b == 0) :
+if (a != 0) or (b != 0) :
   a = a*a
   b = b*b
   print('Вот сумма, разность, произведение и частное соответственно: ', a+b, a-b, a*b, a/b )
